ef00dc4d6ace2091211f794188d63cb6c72905d7
Brian Greco <blgreco72@gmail.com>
2016-02-19 22:21:23 -0500

    Added needed unit-test NuGet packages to all test projects.

---
+++ tests/NetFusion.Core.Tests/Bootstrap/AppContainerTests.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace NetFusion.Core.Tests.Bootstrap
+{
+    public class AppContainerTests
+    {
+        [Fact]
+        public void CreatedAppContainerCanBeReferenced()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
