1f583ef2befc215eddce5766d7d6c106a4804c6a
Brian Greco <blgreco72@gmail.com>
2016-04-16 10:55:16 -0400

    Renamed Eventing project and folder to Messaging to match namespace.

--- src/NetFusion/NetFusion.Messaging/Properties/AssemblyInfo.cs
+++ src/NetFusion/NetFusion.Messaging/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NetFusion.Eventing")]
+[assembly: AssemblyTitle("NetFusion.Messaging")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NetFusion.Eventing")]
+[assembly: AssemblyProduct("NetFusion.Messaging+")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
