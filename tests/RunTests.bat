4b67e7c6754893b9d8cf896a8c714eece84dee42
Brian Greco <blgreco72@gmail.com>
2016-04-17 13:03:46 -0400

    Added command file to run unit tests from shell.  Also added appveyor configuration.

---
+++ tests/RunTests.bat
@@ -0,0 +1,3 @@
+..\packages\xunit.runner.console.2.1.0\tools\xunit.console NetFusion.Tests.Core\bin\Debug\NetFusion.Tests.Core.dll
+..\packages\xunit.runner.console.2.1.0\tools\xunit.console NetFusion.Tests.Infrastructure\bin\Debug\NetFusion.Tests.Infrastructure.dll
+..\packages\xunit.runner.console.2.1.0\tools\xunit.console NetFusion.Tests.Integration\bin\Debug\NetFusion.Tests.Integration.dll
