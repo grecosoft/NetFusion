d7ba4456cf70b1b98b3913c9501a24b280d2157f
Brian Greco <blgreco72@gmail.com>
2017-09-13 06:58:30 -0400

    Created solution folder for plug-in consisting of multiple assemblies.

--- create-packages.cmd
+++ create-packages.cmd
@@ -1,3 +1,3 @@
-msbuild "/t:Restore;Pack" .\NetFusion.sln /p:VersionPrefix=2.0.71
-for /r ./src %%x in (*.2.0.71.nupkg) do copy "%%x" ..\_packages
-for /r ./src %%x in (*.2.0.71.symbols.nupkg) do copy "%%x" ..\_packages
+msbuild "/t:Restore;Pack" .\NetFusion.sln /p:VersionPrefix=2.0.72
+for /r ./src %%x in (*.2.0.72.nupkg) do copy "%%x" ..\_packages
+for /r ./src %%x in (*.2.0.72.symbols.nupkg) do copy "%%x" ..\_packages
