6a659d463f733a26363589d6350383e397ed9b95
Brian Greco <blgreco72@gmail.com>
2018-02-27 18:20:40 -0500

    Minor refactor to logging + Bug fix...
    Modified the LogDetail MS ILogger extensions that add to the base set of log methods provided by MS.  These are the methods that accept a detail object that is serialized to JSON and included as part o fthe log.  Modified all of the methods declarations to only take the detail object and removed the arguments parameter.  Don't see a need for both and this address the bug that was discovered in the logging.  Also changing build file so a commit will not be triggered for a change to read-me files.

--- src/Core/NetFusion.Bootstrap/Logging/LoggerExtensions.cs
+++ src/Core/NetFusion.Bootstrap/Logging/LoggerExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Internal;
 using NetFusion.Base.Exceptions;
 using NetFusion.Common.Extensions;
 using System;
@@ -12,50 +11,52 @@ namespace NetFusion.Bootstrap.Logging
     /// </summary>
     public static class LoggerExtensions
     {
-        private static readonly Func<object, Exception, string> _messageFormatter = (obj, ex) => obj.ToString();
-
         //------------------------------------------DEBUG------------------------------------------//
 
         public static void LogDebugDetails(this ILogger logger, EventId eventId, Exception exception,
-            string message, object details, params object[] args)
+            string message, object details)
         {
             if (logger == null) throw new ArgumentNullException(nameof(logger), "Logger cannot be null.");
-            logger.LogDetails(LogLevel.Debug, eventId, exception, message, args, details);
+            if (exception == null) throw new ArgumentNullException(nameof(exception), "Exception cannot be null.");
+
+            logger.LogDetails(LogLevel.Debug, eventId, exception, message, details);
         }
 
         public static void LogDebugDetails(this ILogger logger, EventId eventId,
-            string message, object details, params object[] args)
+            string message, object details)
         {
             if (logger == null) throw new ArgumentNullException(nameof(logger), "Logger cannot be null.");
-            logger.LogDetails(LogLevel.Debug, eventId, null, message, args, details);
+            logger.LogDetails(LogLevel.Debug, eventId, null, message, details);
         }
 
-        public static void LogDebugDetails(this ILogger logger, string message, object details, params object[] args)
+        public static void LogDebugDetails(this ILogger logger, string message, object details)
         {
             if (logger == null) throw new ArgumentNullException(nameof(logger), "Logger cannot be null.");
-            logger.LogDetails(LogLevel.Debug, 0, null, message, args, details);
+            logger.LogDetails(LogLevel.Debug, 0, null, message, details);
         }
 
         //------------------------------------------TRACE------------------------------------------//
 
         public static void LogTraceDetails(this ILogger logger, EventId eventId, Exception exception,
-            string message, object details, params object[] args)
+            string message, object details)
         {
             if (logger == null) throw new ArgumentNullException(nameof(logger), "Logger cannot be null.");
-            logger.LogDetails(LogLevel.Trace, eventId, exception, message, args, details);
+            if (exception == null) throw new ArgumentNullException(nameof(exception), "Exception cannot be null.");
+
+            logger.LogDetails(LogLevel.Trace, eventId, exception, message, details);
         }
 
         public static void LogTraceDetails(this ILogger logger, EventId eventId,
-            string message, object details, params object[] args)
+            string message, object details)
         {
             if (logger == null) throw new ArgumentNullException(nameof(logger), "Logger cannot be null.");
-            logger.LogDetails(LogLevel.Trace, eventId, null, message, args, details);
+            logger.LogDetails(LogLevel.Trace, eventId, null, message, details);
         }
 
-        public static void LogTraceDetails(this ILogger logger, string message, object details, params object[] args)
+        public static void LogTraceDetails(this ILogger logger, string message, object details)
         {
             if (logger == null) throw new ArgumentNullException(nameof(logger), "Logger cannot be null.");
-            logger.LogDetails(LogLevel.Trace, 0, null, message, args, details);
+            logger.LogDetails(LogLevel.Trace, 0, null, message, details);
         }
 
         public static DurationLogger LogTraceDuration(this ILogger logger, EventId eventId, string processName)
@@ -76,60 +77,67 @@ namespace NetFusion.Bootstrap.Logging
         //------------------------------------------INFORMATION------------------------------------------//
 
         public static void LogInformationDetails(this ILogger logger, EventId eventId, Exception exception,
-            string message, object details, params object[] args)
+            string message, object details)
         {
             if (logger == null) throw new ArgumentNullException(nameof(logger), "Logger cannot be null.");
-            logger.LogDetails(LogLevel.Information, eventId, exception, message, args, details);
+            if (exception == null) throw new ArgumentNullException(nameof(exception), "Exception cannot be null.");
+
+            logger.LogDetails(LogLevel.Information, eventId, exception, message, details);
         }
 
         public static void LogInformationDetails(this ILogger logger, EventId eventId,
-            string message, object details, params object[] args)
+            string message, object details)
         {
             if (logger == null) throw new ArgumentNullException(nameof(logger), "Logger cannot be null.");
-            logger.LogDetails(LogLevel.Information, eventId, null, message, args, details);
+            logger.LogDetails(LogLevel.Information, eventId, null, message, details);
         }
 
-        public static void LogInformationDetails(this ILogger logger, string message, object details, params object[] args)
+        public static void LogInformationDetails(this ILogger logger, string message, object details)
         {
             if (logger == null) throw new ArgumentNullException(nameof(logger), "Logger cannot be null.");
-            logger.LogDetails(LogLevel.Information, 0, null, message, args, details);
+            logger.LogDetails(LogLevel.Information, 0, null, message, details);
         }
 
         //------------------------------------------WARNING------------------------------------------//
 
         public static void LogWarningDetails(this ILogger logger, EventId eventId, Exception exception,
-             string message, object details, object[] args)
+             string message, object details)
         {
             if (logger == null) throw new ArgumentNullException(nameof(logger), "Logger cannot be null.");
-            logger.LogDetails(LogLevel.Warning, eventId, exception, message, args, details);
+            if (exception == null) throw new ArgumentNullException(nameof(exception), "Exception cannot be null.");
+
+            logger.LogDetails(LogLevel.Warning, eventId, exception, message, details);
         }
 
         public static void LogWarningDetails(this ILogger logger, EventId eventId,
-             string message, object details, params object[] args)
+             string message, object details)
         {
             if (logger == null) throw new ArgumentNullException(nameof(logger), "Logger cannot be null.");
-            logger.LogDetails(LogLevel.Warning, eventId, null, message, args, details);
+            logger.LogDetails(LogLevel.Warning, eventId, null, message, details);
         }
 
-        public static void LogWarningDetails(this ILogger logger, string message, object details, params object[] args)
+        public static void LogWarningDetails(this ILogger logger, string message, object details)
         {
             if (logger == null) throw new ArgumentNullException(nameof(logger), "Logger cannot be null.");
-            logger.LogDetails(LogLevel.Warning, 0, null, message, args, details);
+            logger.LogDetails(LogLevel.Warning, 0, null, message, details);
         }
 
         //------------------------------------------ERROR------------------------------------------//
 
         public static void LogErrorDetails(this ILogger logger, EventId eventId, Exception exception,
-            string message, object details, params object[] args)
+            string message, object details)
         {
             if (logger == null) throw new ArgumentNullException(nameof(logger), "Logger cannot be null.");
-            logger.LogDetails(LogLevel.Error, eventId, exception, message, args, details);
+            if (exception == null) throw new ArgumentNullException(nameof(exception), "Exception cannot be null.");
+
+            logger.LogDetails(LogLevel.Error, eventId, exception, message, details);
         }
 
         public static void LogErrorDetails(this ILogger logger, EventId eventId, Exception exception,
-            string message, params object[] args)
+            string message)
         {
             if (logger == null) throw new ArgumentNullException(nameof(logger), "Logger cannot be null.");
+            if (exception == null) throw new ArgumentNullException(nameof(exception), "Exception cannot be null.");
 
             object details = null;
             if (exception is NetFusionException netFusionEx)
@@ -137,63 +145,65 @@ namespace NetFusion.Bootstrap.Logging
                 details = netFusionEx.Details;
             }
 
-            logger.LogDetails(LogLevel.Error, eventId, exception, message, args, details);
-        }
-
-        public static void LogErrorDetails(this ILogger logger, EventId eventId, Exception exception,
-            string message, object details)
-        {
-            if (logger == null) throw new ArgumentNullException(nameof(logger), "Logger cannot be null.");
-            logger.LogDetails(LogLevel.Error, eventId, exception, message, new string[]{}, details);
+            logger.LogDetails(LogLevel.Error, eventId, exception, message, details);
         }
 
         public static void LogErrorDetails(this ILogger logger, EventId eventId,
-            string message, object details, params object[] args)
+            string message, object details)
         {
             if (logger == null) throw new ArgumentNullException(nameof(logger), "Logger cannot be null.");
-            logger.LogDetails(LogLevel.Error, eventId, null, message, args, details);
+            logger.LogDetails(LogLevel.Error, eventId, null, message, details);
         }
 
-        public static void LogErrorDetails(this ILogger logger, string message, object details, params object[] args)
+        public static void LogErrorDetails(this ILogger logger, string message, object details)
         {
             if (logger == null) throw new ArgumentNullException(nameof(logger), "Logger cannot be null.");
-            logger.LogDetails(LogLevel.Error, 0, null, message, args, details);
+            logger.LogDetails(LogLevel.Error, 0, null, message, details);
         }
 
         //------------------------------------------CRITICAL------------------------------------------//
 
         public static void LogCriticalDetails(this ILogger logger, EventId eventId, Exception exception,
-            string message, object details, params object[] args)
+            string message, object details)
         {
             if (logger == null) throw new ArgumentNullException(nameof(logger), "Logger cannot be null.");
-            logger.LogDetails(LogLevel.Critical, eventId, exception, message, args, details);
+            if (exception == null) throw new ArgumentNullException(nameof(exception), "Exception cannot be null.");
+
+            logger.LogDetails(LogLevel.Critical, eventId, exception, message, details);
         }
 
         public static void LogCriticalDetails(this ILogger logger, EventId eventId,
-            string message, object details, params object[] args)
+            string message, object details)
         {
             if (logger == null) throw new ArgumentNullException(nameof(logger), "Logger cannot be null.");
-            logger.LogDetails(LogLevel.Critical, eventId, null, message, args, details);
+            logger.LogDetails(LogLevel.Critical, eventId, null, message, details);
         }
 
-        public static void LogCriticalDetails(this ILogger logger, string message, object details, params object[] args)
+        public static void LogCriticalDetails(this ILogger logger, string message, object details)
         {
             if (logger == null) throw new ArgumentNullException(nameof(logger), "Logger cannot be null.");
-            logger.LogDetails(LogLevel.Critical, 0, null, message, args, details);
+            logger.LogDetails(LogLevel.Critical, 0, null, message, details);
         }
 
         private static void LogDetails(this ILogger logger, LogLevel logLevel, EventId eventId,
-            Exception exception,
-            string message,
-            object[] args,
-            object details)
+           Exception exception,
+           string message,
+           object details)
         {
+            if (string.IsNullOrWhiteSpace(message)) throw new ArgumentException("Log message not specified.", nameof(message));
+            if (details == null) throw new ArgumentNullException(nameof(details), "Log details cannot be null.");
+
             string msgDetails = details?.ToIndentedJson();
 
-            logger.Log(logLevel, eventId, 
-                new FormattedLogValues(message + $" Details: {msgDetails}", args), 
-                exception, 
-                _messageFormatter);
+            logger.Log(logLevel, eventId,
+                message + $" Details: {msgDetails}",
+                exception,
+                FormatMessage);
+        }
+
+        private static string FormatMessage(object state, Exception ex)
+        {
+            return state.ToString();
         }
     }
 }
