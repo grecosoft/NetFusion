e34a75a359ea66605666f0f5d361e5537373d4fd
Brian Greco <brian.greco@captiveaire.com>
2019-05-06 11:11:39 -0400

    common/core unit test refactor complete.

--- netfusion/test/CoreTests/Bootstrap/ConfigurationTests.cs
+++ netfusion/test/CoreTests/Bootstrap/ConfigurationTests.cs
@@ -1,14 +1,7 @@
-﻿/*
-using CoreTests.Bootstrap.Mocks;
+﻿using CoreTests.Bootstrap.Mocks;
 using FluentAssertions;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.Json;
-using NetFusion.Bootstrap.Configuration;
-using NetFusion.Bootstrap.Exceptions;
 using NetFusion.Test.Container;
 using NetFusion.Test.Plugins;
-using System;
-using System.Linq;
 using Xunit;
 
 namespace CoreTests.Bootstrap
@@ -22,35 +15,6 @@ namespace CoreTests.Bootstrap
     public class ConfigurationTests
     {
         /// <summary>
-        /// Host application can provide an instance of a configuration object.  When the container 
-        /// is bootstrapped, each specified configuration is associated with the plug-in instance 
-        /// where defined.  This is only of concern to a plug-in developer needs access to one of
-        /// its defined configurations.
-        /// </summary>
-        [Fact(DisplayName = "Specified Configuration associated with Plug-in")]
-        public void SpecifiedConfiguration_AssociatedWithPlugin()
-        {
-            ContainerFixture.Test(fixture => {
-                fixture.Arrange.Resolver(r =>
-                    {
-                        r.AddPlugin<MockAppHostPlugin>()
-                            .AddPluginType<MockPluginConfig>();
-                    }).Container(c => c.WithConfig(new MockPluginConfig()))
-                
-                    .Assert
-                    .CompositeApp(ca =>
-                    {
-                        ca.AppHostPlugin.PluginConfigs.Should().HaveCount(1);
-                        ca.AppHostPlugin.PluginConfigs.First().Should().BeOfType<MockPluginConfig>();
-                    })
-                    .Plugin<MockAppHostPlugin>(p => {
-                        p.PluginConfigs.Should().HaveCount(1);
-                        p.PluginConfigs.First().Should().BeOfType<MockPluginConfig>();
-                    });
-            });              
-        }
-
-        /// <summary>
         /// When developing a plug-in that has associated configurations, they are most often
         /// accessed from within one or more modules.
         /// </summary>
@@ -58,132 +22,18 @@ namespace CoreTests.Bootstrap
         public void PluginDeveloper_CanAccess_ConfigurationFromModule()
         {
             ContainerFixture.Test(fixture => {
-                fixture.Arrange.Resolver(r =>
+                fixture.Arrange.Container(c =>
                     {
-                        r.AddPlugin<MockAppHostPlugin>()
-                            .AddPluginType<MockPluginOneModule>()
-                            .AddPluginType<MockPluginConfig>();
+                        var hostPlugin = new MockHostPlugin();
+                        hostPlugin.AddConfig<MockPluginConfig>();
+                        hostPlugin.AddModule<MockPluginOneModule>();
+                        
+                        c.RegisterPlugins(hostPlugin);
                     })
-                    .Container(c => c.WithConfig(new MockPluginConfig()))
                     .Assert.PluginModule<MockPluginOneModule>(m => {
                         m.Context.Plugin.GetConfig<MockPluginConfig>().Should().NotBeNull();
                     });
             });
         }
-
-        /// <summary>
-        /// The host application can also provide a configuration by using factory method.   
-        /// </summary>
-        [Fact(DisplayName = "Plug-in Configuration can be initialized using Factory")]
-        public void PluginConfiguration_CanBeInitialized_UsingFactory()
-        {
-            ContainerFixture.Test(fixture => {
-                fixture.Arrange.Resolver(r =>
-                    {
-                        r.AddPlugin<MockAppHostPlugin>()
-                            .AddPluginType<MockPluginConfig>();
-                    })
-                    .Container(c => {
-                        c.WithConfig<MockPluginConfig>((confg) => confg.ConfigValue = "TEST_VALUE");
-                    })     
-                    .Assert.CompositeApp(ca => {
-                        ca.AppHostPlugin.PluginConfigs.Should().HaveCount(1);
-
-                        var config = ca.AppHostPlugin.PluginConfigs.First();
-
-                        config.Should().BeOfType<MockPluginConfig>();
-                        (config as MockPluginConfig).ConfigValue.Should().Be("TEST_VALUE");
-                    });               
-            });               
-        }
-        
-        [Fact(DisplayName = "After Container built Configuration cannot be Added")]
-        public void AfterContainerBuilt_ConfigurationCannotBeAdded()
-        {
-            ContainerFixture.Test(fixture => {
-                fixture.Arrange.Resolver(r =>
-                    {
-                        r.AddPlugin<MockAppHostPlugin>()
-                            .AddPluginType<MockPluginConfig>();
-                    })
-                    .Container(c => c.WithConfig<MockPluginConfig>())
-                    .Act.OnContainer(c =>
-                    {
-                        c.WithConfig<MockPluginConfig>();
-                    })
-                    .Assert.Exception<ContainerException>(e => {
-                        e.Message.Contains("Container has already been built.");
-                    });
-            });
-        }
-
-        /// <summary>
-        /// The application container can also be used by an executable other than a web host.  
-        /// Therefore, the configuration checks both the NETFUSION_ENVIRONMENT and ASPNETCORE_ENVIRONMENT
-        /// variables to determine the current environment of the executing application.  Also, if specified
-        /// for both variables, the NETFUSTION_ENVIROMENT variable is used.
-        /// </summary>
-        [Fact(DisplayName = "Environment Variable can be read to determine Execution Context")]
-        public void EnvironmentVariable_CanBeRead_ToDetermineExecutionContext()
-        {
-            // Clear current environment variables:
-            Environment.SetEnvironmentVariable("NETFUSION_ENVIRONMENT", null);
-            Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", null);
-
-            Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "AspConfigEnv");
-            EnvironmentConfig.EnvironmentName.Should().Be("AspConfigEnv");
-
-            Environment.SetEnvironmentVariable("NETFUSION_ENVIRONMENT", "NetFusionConfigEnv");
-            EnvironmentConfig.EnvironmentName.Should().Be("NetFusionConfigEnv");
-        }
-
-        /// <summary>
-        /// When retrieving a configuration settings, the default configuration is to resolve the 
-        /// value by looking in order the following files:
-        /// appSettings.MachineName.json
-        /// appSettings.Environment.json
-        /// appSettings.Json
-        /// </summary>
-        [Fact(DisplayName = "Configuration files Registered in correct Order")]
-        public void ConfigFilesRegistered_InCorrectOrder()
-        {
-            // Clear current environment variables:
-            Environment.SetEnvironmentVariable("NETFUSION_ENVIRONMENT", null);
-            Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", null);
-
-            Environment.SetEnvironmentVariable("NETFUSION_ENVIRONMENT", "UnitTest");
-            ConfigurationBuilder configBuilder = new ConfigurationBuilder();
-
-            configBuilder.AddAppSettings();
-
-            configBuilder.Sources.Should().HaveCount(3);
-            GetFileAtPosition(configBuilder, 0).Should().Be("appsettings.json");
-            GetFileAtPosition(configBuilder, 1).Should().Be("appsettings.UnitTest.json");
-            GetFileAtPosition(configBuilder, 2).Should().Be($"appsettings.{Environment.MachineName}.json");
-
-
-            string GetFileAtPosition(ConfigurationBuilder builder, int position)
-                => builder.Sources.OfType<JsonConfigurationSource>().ElementAt(position)?.Path ?? "";
-        }
-
-        /// <summary>
-        /// Not part of NetFusion but environment variables can be read as configuration settings
-        /// if the AddEnvironmentVariables method is called on the configuration builder.
-        /// </summary>
-        [Fact(DisplayName = "Configuration Values can read from Environment Variables")]
-        public void ConfigurationValues_CanReadFrom_EnvironmentVariables()
-        {
-            var expectedValue = Guid.NewGuid().ToString();
-
-            Environment.SetEnvironmentVariable("TEST_ENV_VAR", expectedValue);
-
-            var configBuilder = new ConfigurationBuilder();
-            configBuilder.AddEnvironmentVariables();
-            configBuilder.AddAppSettings();
-
-            var config = configBuilder.Build();
-            config["TEST_ENV_VAR"].Should().NotBeNull();
-        }
     }
 }
-*/
--- netfusion/test/CoreTests/Domain/EntitySerializationTests.cs
+++ netfusion/test/CoreTests/Domain/EntitySerializationTests.cs
@@ -1,59 +1,33 @@
-﻿//using FluentAssertions;
-//using NetFusion.Common.Serialization;
-//using NetFusion.Tests.Core.Domain.Mocks;
-//using Newtonsoft.Json;
-//using Xunit;
+﻿using CommonTests.Base.Entity;
+using FluentAssertions;
+using Newtonsoft.Json;
+using Xunit;
 
-//namespace NetFusion.Tests.Core.Domain
-//{
-//    public class EntitySerializationTests
-//    {
-//        [Fact]
-//        public void CanSerializeAndDeserializeAttributedEntity_Binary()
-//        {
-//            var entity = new DynamicEntity();
+namespace CoreTests.Domain
+{
+    public class EntitySerializationTests
+    {
+        [Fact]
+        public void CanSerializeAndDeserializeAttributedEntity_JSON()
+        {
+            var entity = new DynamicEntity();
 
-//            entity.MaxValue = 1000; // static property.
-//            entity.Attributes.Values.Value1 = "ABC";  // dynamic attribute values
-//            entity.Attributes.Values.Value2 = 123;
-//            entity.Attributes.Values.Value3 = new[] { 10, 20 };
+            entity.MaxValue = 1000; // static property.
+            entity.Attributes.Values.Value1 = "ABC";  // dynamic attribute values
+            entity.Attributes.Values.Value2 = 123;
 
-//            var stream = BinaryFormatterUtility.Serialize(entity);
-//            var deserializedEntity = BinaryFormatterUtility.Deserialize<DynamicEntity>(stream);
+            var jsonVal = JsonConvert.SerializeObject(entity);
+            var deserializedEntity = JsonConvert.DeserializeObject<DynamicEntity>(jsonVal);
 
-//            deserializedEntity.Should().NotBeNull();
-//            deserializedEntity.MaxValue.Should().Be(entity.MaxValue);
+            deserializedEntity.Should().NotBeNull();
+            deserializedEntity.MaxValue.Should().Be(entity.MaxValue);
 
-//            var value1 = (string)deserializedEntity.Attributes.Values.Value1;
-//            var value2 = (int)deserializedEntity.Attributes.Values.Value2;
-//            var value3 = (int[])deserializedEntity.Attributes.Values.Value3;
+            var value1 = (string)deserializedEntity.Attributes.Values.Value1;
+            var value2 = (int)deserializedEntity.Attributes.Values.Value2;
 
-//            value1.Should().Be("ABC");
-//            value2.Should().Be(123);
-//            value3.Should().Equal(new int[] { 10, 20 });
-//        }
+            value1.Should().Be("ABC");
+            value2.Should().Be(123);
+        }
 
-//        [Fact]
-//        public void CanSerializeAndDeserializeAttributedEntity_JSON()
-//        {
-//            var entity = new DynamicEntity();
-
-//            entity.MaxValue = 1000; // static property.
-//            entity.Attributes.Values.Value1 = "ABC";  // dynamic attribute values
-//            entity.Attributes.Values.Value2 = 123;
-
-//            var jsonVal = JsonConvert.SerializeObject(entity);
-//            var deserializedEntity = JsonConvert.DeserializeObject<DynamicEntity>(jsonVal);
-
-//            deserializedEntity.Should().NotBeNull();
-//            deserializedEntity.MaxValue.Should().Be(entity.MaxValue);
-
-//            var value1 = (string)deserializedEntity.Attributes.Values.Value1;
-//            var value2 = (int)deserializedEntity.Attributes.Values.Value2;
-
-//            value1.Should().Be("ABC");
-//            value2.Should().Be(123);
-//        }
-
-//    }
-//}
+    }
+}
