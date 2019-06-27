44164071e75e7a81f5fc37fd95f613321fcb4b85
Brian Greco <brian.greco@captiveaire.com>
2019-06-27 17:54:41 -0400

    Code Review: NetFusion.Settings

--- netfusion/src/Core/NetFusion.Settings/SettingsValidationException.cs
+++ netfusion/src/Core/NetFusion.Settings/SettingsValidationException.cs
@@ -4,10 +4,24 @@ using NetFusion.Common.Extensions;
 
 namespace NetFusion.Settings
 {
+    /// <summary>
+    /// Exception thrown when configured settings fail validation.
+    /// </summary>
     public class SettingsValidationException : Exception
     {
+        /// <summary>
+        /// The type of the invalid settings class.
+        /// </summary>
         public Type SettingsType { get; }
+        
+        /// <summary>
+        /// The section path from which the settings were loaded.
+        /// </summary>
         public string SettingsSection { get; }
+        
+        /// <summary>
+        /// The details of the validation.
+        /// </summary>
         public ValidationResultSet ValidationResults { get; }
         
         public SettingsValidationException(Type settingsType, string settingSection, 
