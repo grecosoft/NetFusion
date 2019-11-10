a9e69b09c0529c3fa91efcf90083afe98e723dc5
grecosoft <blgreco72@gmail.com>
2019-11-10 13:44:42 -0500

    Modifed the CorrelationEnricher so the CorrelationId is unique per message.  Then added new property named ScopedRequestId that is unique per request.  This can be used if need to determine which messages were part of the same physical request.

--- netfusion/src/Core/NetFusion.Messaging/Enrichers/CorrelationEnricher.cs
+++ netfusion/src/Core/NetFusion.Messaging/Enrichers/CorrelationEnricher.cs
@@ -11,11 +11,13 @@ namespace NetFusion.Messaging.Enrichers
     {
         //The MessageEnricherModule registers all enrichers with a scoped lifetime,
         // So the below guid value is unique per request.
-        private readonly Guid _scopedCorrelationId = Guid.NewGuid();
+        private readonly Guid _scopedRequestId = Guid.NewGuid();
         
         public override Task Enrich(IMessage message)
         {
-            message.SetCorrelationId(_scopedCorrelationId.ToString());            
+            message.SetCorrelationId(Guid.NewGuid().ToString());    
+            AddMessageProperty(message, "ScopedRequestId", _scopedRequestId);
+            
             return base.Enrich(message);
         }
     }
