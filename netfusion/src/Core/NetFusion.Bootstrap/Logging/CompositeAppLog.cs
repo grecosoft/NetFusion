3ef4cc06a63b2fcf3969968209a7968652f0ef0a
grecosoft <blgreco72@gmail.com>
2019-12-16 16:09:09 -0500

    Removed namespace reference.

--- netfusion/src/Core/NetFusion.Bootstrap/Logging/CompositeAppLog.cs
+++ netfusion/src/Core/NetFusion.Bootstrap/Logging/CompositeAppLog.cs
@@ -4,7 +4,6 @@ using NetFusion.Bootstrap.Plugins;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using NetFusion.Common.Extensions;
 
 namespace NetFusion.Bootstrap.Logging
 {
