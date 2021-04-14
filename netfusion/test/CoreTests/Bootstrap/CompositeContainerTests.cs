b1cb624601f211ce7ef2f5b847418de63810b268
Brian Greco <blgreco72dev@gmail.com>
2021-04-13 21:09:33 -0400

    Additional Unit-Test

--- netfusion/test/CoreTests/Bootstrap/CompositeContainerTests.cs
+++ netfusion/test/CoreTests/Bootstrap/CompositeContainerTests.cs
@@ -3,7 +3,13 @@ using NetFusion.Test.Container;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 using CoreTests.Bootstrap.Mocks;
+using Microsoft.Extensions.Configuration;
+using NetFusion.Base.Logging;
+using NetFusion.Base.Scripting;
+using NetFusion.Base.Serialization;
+using NetFusion.Base.Validation;
 using NetFusion.Bootstrap.Container;
+using NetFusion.Builder;
 using NetFusion.Test.Plugins;
 
 namespace CoreTests.Bootstrap
@@ -13,21 +19,18 @@ namespace CoreTests.Bootstrap
     /// the dependency injection container.  In addition, specific convention based
     /// components will also be registered. 
     /// </summary>
-    public class RegistrationTests
+    public class CompositeContainerTests
     {
         /// <summary>
         /// The application container is also registered within the dependency
         /// injection container as a service.
         /// </summary>
-        [Fact(DisplayName = "Application Container registered as Singleton Service")]
+        [Fact]
         public void CompositeApplication_Registered_AsSingletonService()
         {
             ContainerFixture.Test(fixture =>
             {
-                fixture.Arrange.Container(c =>
-                    {
-                        c.RegisterPlugin<MockHostPlugin>();
-                    })
+                fixture.Arrange.Container(c => { c.RegisterPlugin<MockHostPlugin>(); })
                     .Assert.Services(s =>
                     {
                         var compositeApp = s.GetService<ICompositeApp>();
@@ -41,8 +44,8 @@ namespace CoreTests.Bootstrap
         /// IPluginModuleService interface will automatically be registered as
         /// a service within the dependency-injection container.
         /// </summary>
-        [Fact(DisplayName = "Module Registered as Service if implements Marker interface")]
-        public void ModuleRegisteredAsService_IfImplements_MarkerInterface()
+        [Fact]
+        public void ModuleRegisteredAsService_Implements_MarkerInterface()
         {
             ContainerFixture.Test(fixture =>
             {
@@ -50,7 +53,7 @@ namespace CoreTests.Bootstrap
                     {
                         var testPlugin = new MockHostPlugin();
                         testPlugin.AddModule<MockPluginOneModule>();
-                        
+
                         c.RegisterPlugins(testPlugin);
                     })
                     .Assert.Services(s =>
@@ -59,8 +62,82 @@ namespace CoreTests.Bootstrap
                         moduleAsService.Should().NotBeNull();
                         moduleAsService.Should().BeOfType<MockPluginOneModule>();
                     });
-
             });
+        }
+
+        /// <summary>
+        /// The CompositeContainerBuilder populates Microsoft's IServiceCollection with
+        /// service components.  A reference to the CompositeContainerBuilder is obtained
+        /// by calling an IServiceCollection extension method.
+        /// </summary>
+        [Fact]
+        public void CompositeContainerBuilder_CreatedFrom_ServiceCollection()
+        {
+            IServiceCollection services = new ServiceCollection();
+            IConfiguration configuration = new ConfigurationBuilder().Build();
+            
+            services.AddLogging();
+            
+            var builder = services.CompositeContainer(configuration, new NullExtendedLogger());
+            var hostPlugin = new MockHostPlugin();
+            var corePlugin = new MockCorePlugin();
+
+            builder.AddPlugin(hostPlugin, corePlugin);
+            builder.Compose();
+
+            var provider = services.BuildServiceProvider();
+            var compositeApp = provider.GetRequiredService<ICompositeApp>();
+
+            compositeApp.Should().NotBeNull();
+            CompositeApp.Instance.Should().NotBeNull();
+        }
+
+        /// <summary>
+        /// The host can override plugin specific settings by calling the InitPluginConfig
+        /// generic method passing the configuration type to be updated.
+        /// </summary>
+        [Fact]
+        public void CompositeContainerBuilder_UsedTo_SetPluginConfigurations()
+        {
+            IServiceCollection services = new ServiceCollection();
+            IConfiguration configuration = new ConfigurationBuilder().Build();
+            
+            services.AddLogging();
+            
+            var builder = services.CompositeContainer(configuration, new NullExtendedLogger());
+            var hostPlugin = new MockHostPlugin();
+            var corePlugin = new MockCorePlugin();
+            
+            corePlugin.AddConfig<MockPluginConfigOne>();
+            builder.AddPlugin(hostPlugin, corePlugin);
+
+            builder.InitPluginConfig<MockPluginConfigOne>(c => c.ConfigValue = "10000");
+            corePlugin.GetConfig<MockPluginConfigOne>().ConfigValue.Should().Be("10000");
+            
+            builder.Compose();
+        }
+
+        /// <summary>
+        /// Certain global services such as serialization have default implementations
+        /// registered.
+        /// </summary>
+        [Fact]
+        public void CompositeContainerBuilder_Registers_DefaultServices()
+        {
+            IServiceCollection services = new ServiceCollection();
+            IConfiguration configuration = new ConfigurationBuilder().Build();
+
+            var builder = services.CompositeContainer(configuration, new NullExtendedLogger());
+            var hostPlugin = new MockHostPlugin();
+
+            builder.AddPlugin(hostPlugin);
+            builder.Compose();
+
+            var provider = services.BuildServiceProvider();
+
+            provider.GetRequiredService<IValidationService>();
+            provider.GetRequiredService<ISerializationManager>();
+            provider.GetRequiredService<IEntityScriptingService>();
         }
     }
 }
