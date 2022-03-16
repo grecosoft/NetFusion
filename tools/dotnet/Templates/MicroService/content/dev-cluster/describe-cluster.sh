ae03d731e0e7793d6b29d44b60605dff9afaed32
Brian Greco <blgreco72@gmail.com>
2022-03-16 18:32:54 -0400

    made correction to describe-cluster script

--- tools/dotnet/Templates/MicroService/content/dev-cluster/describe-cluster.sh
+++ tools/dotnet/Templates/MicroService/content/dev-cluster/describe-cluster.sh
@@ -12,6 +12,13 @@ echo "OPEN:   http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/servi
 echo "TOKEN:  "$dashboard_token_value
 
 
+echo ""
+echo "-------------------------------------------------------"
+echo "COMPLETE THE FOLLOWING TO VIEW THE SEQ DASHBOARD"
+echo "-------------------------------------------------------"
+echo "RUN:    kubectl port-forward \"service/seq\" -n [nf:kube-namespace] 8081:80"
+echo "OPEN:   http://localhost:8081"
+
 rabbit_username="$(kubectl get secret microservice-integration-default-user -n [nf:kube-namespace] -o jsonpath='{.data.username}' | base64 --decode)"
 rabbit_password="$(kubectl get secret microservice-integration-default-user -n [nf:kube-namespace] -o jsonpath='{.data.password}' | base64 --decode)"
 
@@ -19,7 +26,7 @@ echo ""
 echo "-------------------------------------------------------"
 echo "COMPLETE THE FOLLOWING TO VIEW THE RABBITMQ DASHBOARD"
 echo "-------------------------------------------------------"
-echo "RUN:    kubectl port-forward "service/microservice-integration" 15672"
+echo "RUN:    kubectl port-forward \"service/microservice-integration\" -n [nf:kube-namespace] 15672"
 echo "OPEN:   http://localhost:15672"
 echo "USER NAME:  "$rabbit_username
 echo "PASSWORD:   "$rabbit_password
