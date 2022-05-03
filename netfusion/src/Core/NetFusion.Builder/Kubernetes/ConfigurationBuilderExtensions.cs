9f6819a25fa99267ee2894ab9f63b41cd91ec8d8
Brian Greco <blgreco72dev@gmail.com>
2022-05-03 16:01:09 -0400

    Adjusted adding Kubernetes Volumes

--- netfusion/src/Core/NetFusion.Builder/Kubernetes/ConfigurationBuilderExtensions.cs
+++ netfusion/src/Core/NetFusion.Builder/Kubernetes/ConfigurationBuilderExtensions.cs
@@ -1,6 +1,9 @@
 using System;
 using System.IO;
+using System.Linq;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using NetFusion.Common.Extensions.Collections;
 
 namespace NetFusion.Builder.Kubernetes;
 
@@ -9,14 +12,14 @@ public static class ConfigurationBuilderExtensions
     /// <summary>
     /// Called when configuring the host for a microservice to read configurations and secrets
     /// from volumes mounted to Kubernetes ConfigMaps and Secrets.  If the specified configuration
-    /// mounted path exists, all existing configuration providers are cleared so configurations and
-    /// secrets are exclusively loaded from all the mounted Kubernetes volumes.  
+    /// mounted path exists, all existing JOSN configuration providers are cleared so configurations
+    /// and secrets are exclusively loaded from all the mounted Kubernetes volumes.  
     /// </summary>
     /// <param name="configurationBuilder">Reference to the host's configuration builder.</param>
     /// <param name="configMountPath">The mounted path containing configurations.</param>
     /// <param name="secretMountPath">The optional mounted path containing secrets.</param>
     /// <returns>Returns to configuration builder.</returns>
-    public static IConfigurationBuilder AddVolumeMounts(
+    public static IConfigurationBuilder AddVolumeMounts2(
         this IConfigurationBuilder configurationBuilder,
         string configMountPath,
         string secretMountPath = null)
@@ -27,27 +30,23 @@ public static class ConfigurationBuilderExtensions
             throw new ArgumentException("Configuration mount path not specified.", nameof(configMountPath));
 
         var configMountDirectory = new DirectoryInfo(configMountPath);
-        if (!configMountDirectory.Exists)
+        if (configMountDirectory.Exists)
         {
-            return configurationBuilder;
+            configurationBuilder.Sources.OfType<JsonConfigurationSource>().ToArray()
+                .ForEach(configSrc =>
+                {
+                    configurationBuilder.Sources.Remove(configSrc);
+                });
+            
+            AddConfigurationFiles(configurationBuilder, configMountDirectory);
         }
         
-        // Clear all existing sources so development settings are not accidentally
-        // used.  Note, the Docker image containing the microservice should never
-        // include configurations - should be listed in .dockerignore.
-        configurationBuilder.Sources.Clear();
-        
-        AddConfigurationFiles(configurationBuilder, configMountDirectory);
-
         if (! string.IsNullOrWhiteSpace(secretMountPath))
         {
             var secretMountDirectory = new DirectoryInfo(secretMountPath);
             AddConfigurationFiles(configurationBuilder, secretMountDirectory);
         }
-        
-        // Add environment variables last so they can override any loaded configurations
-        // loaded from file:
-        configurationBuilder.AddEnvironmentVariables();
+
         return configurationBuilder;
     }
     
