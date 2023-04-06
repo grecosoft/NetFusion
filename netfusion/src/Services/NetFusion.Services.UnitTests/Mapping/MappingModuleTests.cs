53111ad20453ec4b23c27845b2736f0ec5ee7935
Brian Greco <blgreco72dev@gmail.com>
2023-04-06 19:08:23 -0400

    Deleted unit test

--- netfusion/src/Services/NetFusion.Services.UnitTests/Mapping/MappingModuleTests.cs
+++ netfusion/src/Services/NetFusion.Services.UnitTests/Mapping/MappingModuleTests.cs
@@ -11,50 +11,6 @@ namespace NetFusion.Services.UnitTests.Mapping;
 public class MappingModuleTests
 {
     /// <summary>
-    /// One or more IMappingStrategyFactory types can be defined.  When the MappingModule bootstraps,
-    /// it finds all the factories and executes their GetStrategies method to get the created mapping
-    /// strategy instances.  These instances are cached and are stored within the Service Container.
-    /// If a mapping strategy needs to inject a dependency, then a specific strategy should be defined
-    /// which will be found and added to the service container (see next 2 unit tests);
-    /// </summary>
-    [Fact]
-    public void AllMappingStrategyFactories_AreFound()
-    {
-        // var testPlugin = new MockHostPlugin();
-        // testPlugin.AddPluginType<TestMappingStrategyFactory>();
-        //
-        // ContainerFixture.Test(fixture =>
-        // {
-        //     fixture.Arrange
-        //         .Container(c =>
-        //         {
-        //             c.RegisterPlugins(testPlugin);
-        //             c.RegisterPlugin<MappingPlugin>();
-        //         })
-        //         .Assert.PluginModule((MappingModule m) =>
-        //         {
-        //             Assert.NotNull(m.StrategyFactories);
-        //             Assert.Single(m.StrategyFactories);           
-        //             Assert.Equal(1, m.SourceTypeMappings.Count);
-        //                 
-        //             // .. Get the list of possible target types for the source type:
-        //             Assert.True(m.SourceTypeMappings.Contains(typeof(TestMapTypeOne)));
-        //             var targetMappings = m.SourceTypeMappings[typeof(TestMapTypeOne)].ToArray();
-        //         
-        //             Assert.NotNull(targetMappings);           
-        //             Assert.Single(targetMappings);
-        //
-        //             // ... Any strategies returned from a factory are cached:
-        //             var targetMapping = targetMappings.First();
-        //             Assert.NotNull(targetMapping.StrategyInstance);
-        //         
-        //             Assert.True(targetMapping.SourceType == typeof(TestMapTypeOne));
-        //             Assert.True(targetMapping.TargetType == typeof(TestMapTypeTwo));
-        //         });
-        // });
-    }
-
-    /// <summary>
     /// All IMappingStrategy types are found when the module bootstraps.  However, unlike the strategies
     /// returned from a factory, they are not created and cached.  The mapping strategy instance is created
     /// from the service collection when needed by the current request scope.
